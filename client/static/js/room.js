0ba6e649d79b760d31e98f38b939e42b0d846901
Lucas Nogueira <lnogueir@uwaterloo.ca>
2021-01-16 04:05:31 -0500

    commenting out janus for muller

--- client/static/js/room.js
+++ client/static/js/room.js
@@ -7,32 +7,31 @@ socket.on('connect', () => {
   console.log('connected just fine');
 });
 
-// let janus;
+let janus;
+navigator.mediaDevices.getUserMedia({audio: true, video: true})
+.then((stream) => {
+  // Janus.init({
+  //   debug: 'all',
+  //   callback() {
+  //     janus = new Janus({
+  //       server: 'http://localhost:8088/janus',
+  //       iceServers: [
+  //         { url: 'stun:stun.l.google.com:19302' },
+  //         { url: 'stun:stun1.l.google.com:19302' },
+  //         { url: 'stun:stun2.l.google.com:19302' },
+  //       ],
+  //       success() {
+  //         publishFeed(stream);
+  //       },
+  //     });
+  //   },
+  // });
+})
+.catch(() => {
+  alert('Media constraints not satisfied.')
+});
 
-// navigator.mediaDevices.getUserMedia({audio: true, video: true})
-// .then((stream) => {
-//   Janus.init({
-//     debug: 'all',
-//     callback() {
-//       janus = new Janus({
-//         server: 'http://localhost:8088/janus',
-//         iceServers: [
-//           { url: 'stun:stun.l.google.com:19302' },
-//           { url: 'stun:stun1.l.google.com:19302' },
-//           { url: 'stun:stun2.l.google.com:19302' },
-//         ],
-//         success() {
-//           publishFeed(stream);
-//         },
-//       });
-//     },
-//   });
-// })
-// .catch(() => {
-//   alert('Media constraints not satisfied.')
-// });
+function publishStream(stream) {
 
-// function publishStream(stream) {
-
-// }
+}
 
