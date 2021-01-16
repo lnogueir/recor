53cc4a1323e4085656a9c52803a53c510d210937
muller <lukasmullerdeoliveira@gmail.com>
2021-01-16 15:16:26 -0300

    code that sends the image in every 2 seconds

--- client/static/js/room.js
+++ client/static/js/room.js
@@ -1,6 +1,9 @@
 let userName = prompt('Enter your name: ');
 const roomId = parseInt(document.querySelector('#roomId').value);
 const socket = io({query: { participantName: userName }});
+const API_URL = '/api/emotion_handler'
+const video = document.querySelector('video');
+// const canvas = document.querySelector("#video-container > canvas");
 
 var recognition = new webkitSpeechRecognition() || SpeechRecognition();
 
@@ -20,38 +23,70 @@ if (typeof recognition === 'undefined') {
   };
 }
 
-
-socket.on('connect', () => {
+socket.on('connect', async () => {
   console.log('connected just fine');
+  // captureLoop()
 });
 
+var constraints = { audio: true, video: { width: 1280, height: 720 } }; 
 // let janus;
 
-navigator.mediaDevices.getUserMedia({audio: true, video: true})
-.then((stream) => {
+// if (navigator.mediaDevices.getUserMedia){
+//   navigator.mediaDevices.getUserMedia(constraints)
+//   .then((stream) => {
+//     video.srcObject = stream;
+//     video.onloadedmetadata = function(e) {
+//       // video.play();
+      
+//     };
+//     // imgCaptured= capture()
+//     // socket.emit('emotion',await imgCaptured)
+//     // recognition.start();
+    
+//     // Janus.init({
+//     //   debug: 'all',
+//     //   callback() {
+//     //     janus = new Janus({
+//     //       server: 'http://localhost:8088/janus',
+//     //       iceServers: [
+//     //         { url: 'stun:stun.l.google.com:19302' },
+//     //         { url: 'stun:stun1.l.google.com:19302' },
+//     //         { url: 'stun:stun2.l.google.com:19302' },
+//     //       ],
+//     //       success() {
+//     //         publishFeed(stream);
+//     //       },
+//     //     });
+//     //   },
+//     // });
+//   })
+//   .catch(() => {
+//     alert('Media constraints not satisfied.')
+//   });
+// }
 
-  recognition.start();
+async function captureLoop(){
+    imgCaptured= await capture()
+    // console.log(imgCaptured)
+    socket.emit('emotion',imgCaptured)
+  
+    setTimeout(await captureLoop, 2000);
+}
 
-  // Janus.init({
-  //   debug: 'all',
-  //   callback() {
-  //     janus = new Janus({
-  //       server: 'http://localhost:8088/janus',
-  //       iceServers: [
-  //         { url: 'stun:stun.l.google.com:19302' },
-  //         { url: 'stun:stun1.l.google.com:19302' },
-  //         { url: 'stun:stun2.l.google.com:19302' },
-  //       ],
-  //       success() {
-  //         publishFeed(stream);
-  //       },
-  //     });
-  //   },
-  // });
-})
-.catch(() => {
-  alert('Media constraints not satisfied.')
-});
+
+async function capture() {
+  var canvas = document.querySelector('canvas');
+  canvas.width = 640;
+  canvas.height = 480;
+  var ctx = canvas.getContext('2d');
+
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  
+  imageCapture = await canvas.toDataURL("image/jpeg");
+
+  return imageCapture;
+}
+
 
 function publishStream(stream) {
 
