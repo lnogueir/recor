3fb2b649ec8b17d34e5cc7e6e0d8082ac5e2b015
Lucas Nogueira <lnogueir@uwaterloo.ca>
2021-01-16 20:58:17 -0500

    added back needed libraries

--- sockets/highlight_handles.py
+++ sockets/highlight_handles.py
@@ -2,6 +2,7 @@ from flask import request, session
 from flask_socketio import emit
 from __main__ import socketio
 from datetime import datetime
+import os, sys
 
 parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir_name)
